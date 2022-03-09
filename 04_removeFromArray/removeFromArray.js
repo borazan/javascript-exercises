c2d39009995cade4f4f7cb2227f53b62e440e8e4
borazan <boratuncel@gmail.com>
2022-03-09 19:25:44 +0100

    complete exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray(arr, ...args) {
+  let returnArray = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (args.includes(arr[i])) continue;
+    else returnArray.push(arr[i]);
+  }
+  return returnArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let result = 0;
+    if (typeof(num1) != "number" || typeof(num2) != "number" || num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        let x = num2;
+        num2 = num1;
+        num1 = x;
+    }
+    for(let i = num1; i <= num2; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
